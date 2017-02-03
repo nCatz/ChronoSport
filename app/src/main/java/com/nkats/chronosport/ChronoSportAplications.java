26104dc52ea4a49d69e268e868b12f87d4f0f21d
Amador <amadorfernandez2@gmail.com>
2017-02-03 20:54:30 +0100

    Create DataBaseHelper

---
+++ app/src/main/java/com/nkats/chronosport/ChronoSportAplications.java
@@ -0,0 +1,25 @@
+package com.nkats.chronosport;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by amador on 3/02/17.
+ */
+
+public class ChronoSportAplications extends Application {
+
+
+    private static Context context;
+
+    public static Context getContex(){
+
+        return context;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this;
+    }
+}
---
+++ app/src/main/java/com/nkats/chronosport/database/DataBaseHelper.java
@@ -0,0 +1,109 @@
+package com.nkats.chronosport.database;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+
+import com.nkats.chronosport.ChronoSportAplications;
+
+/**
+ * Created by amador on 3/02/17.
+ */
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "ChronoSport.db";
+    public static DataBaseHelper instance;
+    
+    public static DataBaseHelper getInstance(){
+
+        if(instance == null){
+
+            instance = new DataBaseHelper();
+        }
+
+        return instance;
+    }
+
+    public SQLiteDatabase openDataBase(){
+
+        return getWritableDatabase();
+    }
+
+    public void closeDataBase(){
+
+        close();
+    }
+
+    private DataBaseHelper() {
+        super(ChronoSportAplications.getContex(), DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.beginTransaction();
+
+        try {
+            sqLiteDatabase.execSQL(DatabaseContract.ChronoTimeElemntEntry.SQL_CREATE_ENTRIES);
+            sqLiteDatabase.execSQL(DatabaseContract.ChronoTimeElemntEntry.SQL_CREATE_ENTRIES);
+            sqLiteDatabase.execSQL(DatabaseContract.ChronoEntry.SQL_CREATE_ENTRIES);
+            onCreate(sqLiteDatabase);
+            sqLiteDatabase.setTransactionSuccessful();
+        } catch (SQLException e) {
+
+
+        }finally {
+
+            sqLiteDatabase.endTransaction();
+        }
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        sqLiteDatabase.beginTransaction();
+
+        try {
+            sqLiteDatabase.execSQL(DatabaseContract.ChronoTimeElemntEntry.SQL_DELETE_ENTRIES);
+            sqLiteDatabase.execSQL(DatabaseContract.ChronoTimeElemntEntry.SQL_DELETE_ENTRIES);
+            sqLiteDatabase.execSQL(DatabaseContract.ChronoEntry.SQL_DELETE_ENTRIES);
+            onCreate(sqLiteDatabase);
+            sqLiteDatabase.setTransactionSuccessful();
+        } catch (SQLException e) {
+
+
+        }finally {
+
+            sqLiteDatabase.endTransaction();
+        }
+
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, newVersion, oldVersion);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+
+        super.onOpen(db);
+        if(!db.isReadOnly()){
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
+
+                db.setForeignKeyConstraintsEnabled(true);
+
+            }else {
+
+                db.execSQL("PRAGMA foreign_keys = ON");
+            }
+
+        }
+    }
+}
