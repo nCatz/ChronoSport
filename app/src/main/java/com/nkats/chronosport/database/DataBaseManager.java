24a46be77be9124db5ef07283d48bff8b50c4e69
Amador <amadorfernandez2@gmail.com>
2017-02-03 22:01:19 +0100

    Add synchronized method

---
+++ app/src/main/java/com/nkats/chronosport/database/DataBaseManager.java
@@ -0,0 +1,15 @@
+package com.nkats.chronosport.database;
+
+import com.nkats.chronosport.model.Chrono;
+
+/**
+ * Created by amador on 3/02/17.
+ */
+
+public class DataBaseManager {
+
+    public void addChrono(Chrono chrono){
+
+        
+    }
+}
